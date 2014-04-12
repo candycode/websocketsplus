d43a8104c351db9c6134f8d2a26e750187b354d8
candycode <candykode@gmail.com>
2014-04-12 22:41:36 +0200

    Added touch event support in client

--- src/examples/image-stream/wsmouse.js
+++ src/examples/image-stream/wsmouse.js
@@ -14,17 +14,13 @@ var count = 4; //use this to send only 1/4 of the move events, makes it
 // }​
 
 function sendMouseEvent(m, e) {
-  e.preventDefault();
+  //e.preventDefault();
   if(m == MOUSE_MOVE) if(--count > 0) return;
   e = e || window.event;
   sendBuffer[0] = m;
-  if(m == MOUSE_MOVE || m == MOUSE_UP) {
-    sendBuffer[1] = e.clientX;
-    sendBuffer[2] = e.clientY;
-  } else {
-    sendBuffer[1] = e.clientX;
-    sendBuffer[2] = e.clientY;
-  } 
+  sendBuffer[1] = e.clientX;
+  sendBuffer[2] = e.clientY;
+  
   // if(!websocket || websocket.readyState != websocket.open) {
   //   console.log(sendBuffer[0] + " " +
   //               sendBuffer[1] + " " +
@@ -35,22 +31,45 @@ function sendMouseEvent(m, e) {
   if(m == MOUSE_MOVE) count = 4;
 }
 window.onmousedown = function(e) {
+  e.preventDefault();
   mouseDown = true;
   sendMouseEvent(MOUSE_DOWN, e);                    
 }
 window.onmouseup = function(e) {
+  e.preventDefault();
   mouseDown = false;
   sendMouseEvent(MOUSE_UP, e);                    
 }
 window.onmousemove = function(e) {
+  e.preventDefault();
   if(!mouseDown) return;
   sendMouseEvent(MOUSE_MOVE, e);                    
 }
 
-// window.addEventListener("touchstart", handleStart, false);
-// window.addEventListener("touchend", handleEnd, false);
-// window.addEventListener("touchcancel", handleCancel, false);
-// window.addEventListener("touchleave", handleEnd, false);
-// window.addEventListener("touchmove", handleMove, false);
+function handleStart(e) {
+  //document.querySelector("#pos").textContent = e.touches.length;
+  e.preventDefault();
+  sendMouseEvent(MOUSE_DOWN, e.touches[e.touches.length - 1]);    
+}
+
+function handleEnd(e) {
+  e.preventDefault();
+  sendMouseEvent(MOUSE_UP, e.touches[e.touches.length - 1]);    
+}
+
+function handleMove(e) {
+  e.preventDefault();
+  sendMouseEvent(MOUSE_MOVE, e.touches[e.touches.length - 1]);    
+}
+
+function handleCancel(e) {
+  e.preventDefault();  
+}
+
+window.addEventListener("touchstart", handleStart, false);
+window.addEventListener("touchend", handleEnd, false);
+window.addEventListener("touchcancel", handleCancel, false);
+window.addEventListener("touchleave", handleEnd, false);
+window.addEventListener("touchmove", handleMove, false);
 
 
